cda306420524eae81aa845dd7e98f17c5be68cd2
Kyle Sandell <kasandell@gmail.com>
2015-12-02 22:04:00 -0800

    Initial Commit

---
+++ WebCrawler/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  WebCrawler
+//
+//  Created by Kyle Sandell on 12/2/15.
+//  Copyright © 2015 Kyle Sandell. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
